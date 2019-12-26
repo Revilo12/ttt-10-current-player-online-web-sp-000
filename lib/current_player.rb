bc6179aea850d2da645aa6602fd5c12d4fa6d81b
Oliver Dahl <oliverpdahl@gmail.com>
2019-12-26 22:27:25 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,20 @@
+#Returns the number of turns that have elapsed from a given board
+def turn_count(board)
+  turn = 0
+  board.each do |spot|
+    if spot == " X " || spot == " O "
+      turn += 1 
+    end
+  end
+  turn
+end
+
+#Returns the cahracer of the current player
+def current_player(board)
+  turn  = turn_count(board)
+  if turn % 2  == 0 #is even
+    "X"
+  else
+    "O"
+  end
+end
