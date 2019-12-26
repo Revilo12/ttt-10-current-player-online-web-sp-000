c35a6c61ec93383ed505a53f792a75f64da0d117
Oliver Dahl <oliverpdahl@gmail.com>
2019-12-26 22:44:48 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,10 +1,10 @@
 #Returns the number of turns that have elapsed from a given board
 def turn_count(board)
-  possible_empty = [" ", "", nil]
   turn = 0
-  if spot != " " && spot != "" && spot != nil 
-    turn += 1 
-  end
+  board.each do |spot|
+    if spot != " " && spot != "" && spot != nil 
+      turn += 1 
+    end
   turn
 end
 
